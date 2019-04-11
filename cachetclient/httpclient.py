63b0f0492f95a8cb9609d9e6354084679d54b551
Einar Forselv <eforselv@gmail.com>
2019-04-12 01:34:08 +0200

    Support specifying timeout and user_agent

--- cachetclient/httpclient.py
+++ cachetclient/httpclient.py
@@ -1,3 +1,4 @@
+import json
 from urllib.parse import urljoin
 
 import requests
@@ -5,34 +6,43 @@ import requests
 
 class HttpClient:
 
-    def __init__(self, base_url, api_token, verify_tls=True):
+    def __init__(self, base_url, api_token, timeout=None, verify_tls=True, user_agent=None):
         self.base_url = base_url
-        self.verify_tls = verify_tls
         if not self.base_url.endswith('/'):
             self.base_url += '/'
+        self.verify_tls = verify_tls
+        self.timeout = timeout
+        self.user_agent = user_agent
 
         self._session = requests.Session()
-        self._session.headers.update({'X-Cachet-Token': api_token})
+        self._session.headers.update({
+            'X-Cachet-Token': api_token,
+            'User-Agent': user_agent,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        })
 
-    def get(self, path):
-        return self.request('GET', path)
+    def get(self, path, params=None):
+        return self.request('GET', path, params=params)
 
-    def post(self, path):
-        return self.request('POST', path)
+    def post(self, path, data):
+        return self.request('POST', path, data=data)
 
     def delete(self, path):
         return self.request('DELETE', path)
 
     def request(self, method, path, params=None, data=None):
         url = urljoin(self.base_url, path)
-        result = self._session.request(
+        response = self._session.request(
             method,
             url,
             params=params,
-            data=None,
+            data=json.dumps(data),
             verify=self.verify_tls,
+            timeout=self.timeout,
         )
-        if result.ok:
-            return result
+        if response.ok:
+            return response
 
-        result.raise_for_status()
+        print(response.text)
+        response.raise_for_status()
