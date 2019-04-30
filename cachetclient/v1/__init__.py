6be6c47d5d25bf97d76b5a885f225c5b75d9706b
Einar Forselv <eforselv@gmail.com>
2019-04-30 14:07:06 +0200

    Import resource classes in v1 package

--- cachetclient/v1/__init__.py
+++ cachetclient/v1/__init__.py
@@ -1,1 +1,8 @@
 from cachetclient.v1.client import Client
+
+from cachetclient.v1.subscribers import Subscriber
+from cachetclient.v1.components import Component
+from cachetclient.v1.component_groups import CompontentGroup
+from cachetclient.v1.incident_updates import IndicentUpdate
+from cachetclient.v1.metrics import Metrics
+from cachetclient.v1.metric_points import MetricPoints
