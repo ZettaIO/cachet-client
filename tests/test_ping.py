f3ba47f3edf70a927a3bce0002787ffabf3ec6df
Einar Forselv <eforselv@gmail.com>
2019-05-13 05:24:39 +0200

    Ping test: separate call and get

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -8,11 +8,14 @@ from fakeapi import FakeHttpClient
 @mock.patch('cachetclient.client.HttpClient', new=FakeHttpClient)
 class SubscriberTests(CachetTestcase):
 
-    def test_ping(self):
+    def test_ping_call(self):
+        """Ping using __call__ method"""
         client = self.create_client()
-
         result = client.ping()
         self.assertTrue(result)
 
+    def test_ping_get(self):
+        """Ping using get method"""
+        client = self.create_client()
         result = client.ping.get()
         self.assertTrue(result)
