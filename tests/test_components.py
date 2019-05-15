3a53e8d2397f657c2e7f6a8d6405fee8e4f4bec9
Einar Forselv <eforselv@gmail.com>
2019-05-15 08:51:05 +0200

    Add component tag tests

--- tests/test_components.py
+++ tests/test_components.py
@@ -9,6 +9,17 @@ from cachetclient.v1 import enums
 @mock.patch('cachetclient.client.HttpClient', new=FakeHttpClient)
 class ComponentsTests(CachetTestcase):
 
+    @mock.patch('cachetclient.client.HttpClient', new=FakeHttpClient)
+    def setUp(self):
+        self.client = self.create_client()
+
+    def create_component(self, client):
+        return client.components.create(
+            "API Server",
+            enums.COMPONENT_STATUS_OPERATIONAL,
+            description="General API server",
+        )
+
     def test_count(self):
         """Count components"""
         client = self.create_client()
@@ -16,15 +27,10 @@ class ComponentsTests(CachetTestcase):
 
     def test_create(self):
         """Create and obtain component"""
-        client = self.create_client()
-        client.components.create(
-            "API Server",
-            enums.COMPONENT_STATUS_OPERATIONAL,
-            description="General API server",
-        )
-        self.assertEqual(client.components.count(), 1)
+        self.create_component(self.client)
+        self.assertEqual(self.client.components.count(), 1)
 
-        comp = next(client.components.list())
+        comp = next(self.client.components.list())
         self.assertEqual(comp.id, 1)
         self.assertEqual(comp.name, "API Server")
         self.assertEqual(comp.description, "General API server")
@@ -36,24 +42,33 @@ class ComponentsTests(CachetTestcase):
         self.assertIsNotNone(comp.updated_at)
         self.assertIsNone(comp.deleted_at)
 
-        comp = client.components.get(1)
+        comp = self.client.components.get(1)
         self.assertEqual(comp.id, 1)
 
     def test_delete(self):
         """Create and delete component"""
-        client = self.create_client()
-        client.components.create(
-            "API Server",
-            enums.COMPONENT_STATUS_OPERATIONAL,
-            description="General API server",
-        )
-        self.assertEqual(client.components.count(), 1)
-        comp = next(client.components.list())
+        self.create_component(self.client)
+        self.assertEqual(self.client.components.count(), 1)
+        comp = next(self.client.components.list())
         comp.delete()
-        self.assertEqual(client.components.count(), 0)
+        self.assertEqual(self.client.components.count(), 0)
 
     def test_delete_nonexist(self):
         """Delete non-exsitent component"""
-        client = self.create_client()
         with self.assertRaises(HTTPError):
-            client.components.delete(1337)
+            self.client.components.delete(1337)
+
+    def test_tags(self):
+        """Test tags"""
+        comp = self.create_component(self.client)
+        comp.add_tag('test')
+        self.assertTrue(comp.has_tag('test'))
+        self.assertFalse(comp.has_tag('thing'))
+        comp.del_tag('test')
+        self.assertFalse(comp.has_tag('test'))
+
+        comp.add_tag('tag1')
+        comp.add_tag('tag2')
+        comp = comp.update()
+        self.assertTrue(comp.has_tag('tag1'))
+        self.assertTrue(comp.has_tag('tag2'))
