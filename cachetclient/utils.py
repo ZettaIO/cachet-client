c5218dcd38c23173d35188523d3f30aaa4b2c465
Einar Forselv <eforselv@gmail.com>
2019-05-24 09:36:54 +0200

    Datetime conversion

--- cachetclient/utils.py
+++ cachetclient/utils.py
@@ -2,4 +2,16 @@ from datetime import datetime
 
 
 def to_datetime(timestamp: str):
-    pass
+    """
+    Convert string to datetime of format '2019-05-24 09:26:22'
+
+    Args:
+        timestamp (str): String timestamp
+
+    Returns:
+        datetime if input is a valid datetime string
+    """
+    if timestamp is None:
+        return None
+
+    return datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
