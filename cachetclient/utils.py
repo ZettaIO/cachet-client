1e7dd81348bac1724f117a75db85c9d19221ffff
Einar Forselv <eforselv@gmail.com>
2019-05-24 10:19:48 +0200

    Support the more clunky verified_at datetime format

--- cachetclient/utils.py
+++ cachetclient/utils.py
@@ -1,9 +1,13 @@
+import re
 from datetime import datetime
 
 
 def to_datetime(timestamp: str):
     """
-    Convert string to datetime of format '2019-05-24 09:26:22'
+    Convert string to datetime of formats::
+    
+        '2019-05-24 09:26:22'
+        'Friday 24th May 2019 10:01:44'
 
     Args:
         timestamp (str): String timestamp
@@ -14,4 +18,17 @@ def to_datetime(timestamp: str):
     if timestamp is None:
         return None
 
-    return datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+    try:
+        # '2019-05-24 09:26:22'
+        return datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+    except ValueError:
+        pass
+
+    try:
+        # 'Friday 1st May 2019 10:01:44'. Used in verified_at for subscribers
+        sub_timestamp = re.sub(r"\b([0123]?[0-9])(st|th|nd|rd)\b", r"\1", timestamp)
+        return datetime.strptime(sub_timestamp, '%A %d %B %Y %H:%M:%S')
+    except ValueError:
+        pass
+
+    raise ValueError("datetime string '{}' not supported".format(timestamp))
