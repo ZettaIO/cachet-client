754c6c8a8a6e99c417f649a0373f83f6e45b5b27
Einar Forselv <eforselv@gmail.com>
2019-05-27 17:12:43 +0200

    Set up basic metric resource

--- cachetclient/v1/metrics.py
+++ cachetclient/v1/metrics.py
@@ -2,9 +2,60 @@ from cachetclient.base import Manager, Resource
 
 
 class Metrics(Resource):
-    pass
+
+    @property
+    def id(self) -> int:
+        return self.get('id')
+
+    @property
+    def name(self) -> str:
+        return self.get('name')
+
+    @property
+    def suffix(self) -> str:
+        return self.get('suffix')
+
+    @property
+    def description(self):
+        return self.get('description')
+
+    @property
+    def default_value(self):
+        return self.get('default_value')
+
+    @property
+    def calc_type(self) -> int:
+        return self.get('calc_type')
+
+    @property
+    def display_chart(self) -> int:
+        return self.get('display_chart')
+
+    @property
+    def created_at(self):
+        self.get('created_at')
+
+    @property
+    def updated_at(self):
+        self.get('updated_at')
+
+    @property
+    def default_view_name(self):
+        return self.get('default_view_name')
 
 
 class MetricsManager(Manager):
     resource_class = Metrics
     path = 'metrics'
+
+    def create(self):
+        pass
+
+    def list(self):
+        pass
+
+    def get(self):
+        pass
+
+    def delete(self):
+        pass
