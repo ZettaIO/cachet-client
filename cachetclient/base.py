185ec1937c43ea2973a33684ee1aaf00567944b6
Einar Forselv <eforselv@gmail.com>
2019-06-22 02:26:35 +0200

    Proper docstrings in base

--- cachetclient/base.py
+++ cachetclient/base.py
@@ -1,5 +1,5 @@
 from cachetclient.httpclient import HttpClient
-from typing import Union
+from typing import Generator, Union
 
 
 class Resource:
@@ -10,17 +10,39 @@ class Resource:
 
     @property
     def attrs(self) -> dict:
-        """The raw json respons from the server"""
+        """dict: The raw json respons from the server"""
         return self._data
 
     def get(self, name) -> Union[int, str, float, bool]:
-        """Obtain any attribute name for the resource"""
+        """
+        Safely obtain any attribute name for the resource
+
+        Args:
+            name (str): Key name in json response
+
+        Returns:
+            Value from the raw json response.
+            If the key doesn't exist ``None`` is returned.
+        """
         return self._data.get(name)
 
     def update(self):
+        """
+        Posts the values in the resource to the server.
+
+        Example::
+
+            # Change an attribute and save the resource
+            >> resource.value = something
+            >> updated_resource = resource.update()
+
+        Returns:
+            The updated resource from the server
+        """
         return self._manager.update(self.get('id'), **self.attrs)
 
     def delete(self) -> None:
+        """Deletes the resource from the server"""
         self._manager.delete(self.get('id'))
 
     def __repr__(self) -> str:
@@ -46,15 +68,37 @@ class Manager:
         if self.path is None:
             raise ValueError('path not defined for class {}'.format(self.__class__))
 
-    def _create(self, path, data):
+    def _create(self, path: str, data: dict):
         response = self._http.post(path, data=data)
         return self.resource_class(self, response.json()['data'])
 
-    def _update(self, path, resource_id, data):
+    def _update(self, path: str, resource_id: int, data: dict) -> Resource:
+        """Generic resource updater
+
+        Args:
+            path (str): url path relative to base url
+            resource_id (int): The resource to update
+            data (dict): New data
+
+        Returns:
+            Resource: The updated resource from the server
+        """
         response = self._http.put("{}/{}".format(path, resource_id), data=data)
         return self.resource_class(self, response.json()['data'])
 
-    def _list_paginated(self, path, page=1, per_page=20):
+    def _list_paginated(self, path: str, page=1, per_page=20) -> Generator[Resource, None, None]:
+        """List resources paginated.
+
+        Args:
+            path (str): url path relative to base url
+
+        Keyword Args:
+            page (int): Page to start on
+            per_page (int): Number of entires per page
+
+        Returns:
+            Generator of resources
+        """
         while True:
             result = self._http.get(
                 path,
@@ -81,23 +125,50 @@ class Manager:
     #     result = self._http.get(path, params={'per_page': 1, **params})
     #     json_data = result.json()
 
-    def _get(self, path, resource_id):
+    def _get(self, path: str, resource_id: int):
+        """Generic resource getter (single)
+
+        Args:
+            path (str): url path relative to base url
+            resource_id (int): The resource id to get
+
+        Returns:
+            :py:data:`Resource`: A resource instance
+        """
         result = self._http.get("{}/{}".format(path, resource_id))
         json_data = result.json()
         return self.resource_class(self, json_data['data'])
 
-    def _count(self, path):
+    def _count(self, path: str) -> int:
+        """Generic count method
+        using the pagination system to obtain the total number of resources
+
+        Args:
+            path (str): url path relative to base url
+
+        Returns:
+            int: Number of resources
+        """
         result = self._http.get(path, params={'per_page': 1})
         json_data = result.json()
         return json_data['meta']['pagination']['total']
 
-    def _delete(self, path, resource_id):
+    def _delete(self, path: str, resource_id: int) -> None:
+        """Generic resource deleter
+
+        Args:
+            path (str): url path relative to base url
+            resource_id (int): The resource to delete
+        """
         self._http.delete(path, resource_id)
 
-    def _build_data_dict(self, **kwargs):
-        """
-        Builds a data dictionary for posting to the server.
+    def _build_data_dict(self, **kwargs) -> dict:
+        """Builds a data dictionary for posting to the server.
+
         Will ommit key/value pars with None values.
         This makes partial updates less error prone.
+
+        Returns:
+            dict: dict without `None` values
         """
         return {key: value for key, value in kwargs.items() if value is not None}
