1b8d29608d3e0c57d5502c31cb1e8badd89413c1
Илья Поляков <ipolyakov@mbp16.local>
2020-08-27 16:49:39 +0300

    add schedules

--- cachetclient/v1/schedules.py
+++ cachetclient/v1/schedules.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from cachetclient.v1 import enums
+from typing import Generator, Set
 
 from cachetclient.base import Resource, Manager
 from cachetclient import utils
@@ -9,6 +11,10 @@ class Schedule(Resource):
     @property
     def id(self) -> int:
         return self.get('id')
+
+    @property
+    def name(self) -> str:
+        return self.get('name')
 
     @property
     def message(self) -> str:
@@ -31,14 +37,101 @@ class ScheduleManager(Manager):
     path = 'schedules'
     resource_class = Schedule
 
-    def create(self, *args, **kwargs):
-        raise NotImplementedError("Create schedule not implemented")
+    def create(
+            self,
+            *,
+            name: str,
+            status: int,
+            message: str = None,
+            scheduled_at: datetime = None):
+        """Create a shedule.
 
-    def update(self, *args, **kwargs):
-        raise NotImplementedError("update schedule not implemented")
+        Returns:
+            :py:class:`Shedule` instance
+        """
+        if status not in enums.SCHEDULE_STATUS_LIST:
+            raise ValueError("Invalid status id '{}'. Valid values :{}".format(
+                status,
+                enums.SCHEDULE_STATUS_LIST,
+            ))
 
-    def list(self, *args, **kwargs):
-        raise NotImplementedError("List schedules not implemented")
+        return self._create(
+            self.path,
+            {
+                'name': name,
+                'message': message,
+                'status': status,
+                'scheduled_at': scheduled_at
+            }
+        )
 
-    def delete(self, schedule_id):
-        raise NotImplementedError("Delete schedules not implemented")
+    def update(
+            self,
+            schedule_id: int,
+            *,
+            name: str,
+            status: int,
+            message: str = None,
+            scheduled_at: datetime = None,
+            **kwargs) -> Schedule:
+        """Update a Schedule by id.
+
+        Args:
+            schedule_id (int): The schedule to update
+
+        Keyword Args:
+            status (int): Status of the schedule (see enums)
+            name (str): New name
+            description (str): New description
+
+        Returns:
+            Updated Schedule from server
+        """
+        return self._update(
+            self.path,
+            schedule_id,
+            self._build_data_dict(
+                name=name,
+                status=status,
+                message=message,
+                scheduled_at=scheduled_at
+            ),
+        )
+
+
+    def list(self, page: int = 1, per_page: int = 20) -> Generator[Schedule, None, None]:
+        """List all schedules
+
+        Keyword Args:
+            page (int): The page to start listing
+            per_page (int): Number of entries per page
+
+        Returns:
+            Generator of Schedules instances
+        """
+        yield from self._list_paginated(self.path, page=page, per_page=per_page)
+
+    def get(self, schedule_id: int) -> Schedule:
+        """Get a schedule by id
+
+        Args:
+            schedule_id (int): Id of the schedule
+
+        Returns:
+            Schedule instance
+
+        Raises:
+            HttpError: if not found
+        """
+        return self._get(self.path, schedule_id)
+
+    def delete(self, schedule_id: int) -> None:
+        """Delete a schedule
+
+        Args:
+            schedule_id (int): Id of the schedule
+
+        Raises:
+            HTTPError: if schedule do not exist
+        """
+        self._delete(self.path, schedule_id)
