ada00b09754256121d57fa6adccbd9a32c4a90ab
einarf <eforselv@gmail.com>
2020-09-08 14:22:13 +0200

    Extras setup for live runs (2.3 and 2.4)

---
+++ extras/env23.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export CACHET_ENDPOINT='http://localhost:8000/api/v1'
+export CACHET_API_TOKEN='xwqW5J9IjvC8ERvf3GdU'
---
+++ extras/env24.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export CACHET_ENDPOINT='http://localhost:8080/api/v1'
+export CACHET_API_TOKEN='bZi1aeZKq3HSNL8XzKFk'
