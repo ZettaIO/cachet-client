bc4e8b3b881afbd892c39c90cd3a7558542a80f2
einarf <eforselv@gmail.com>
2020-09-11 15:52:52 +0200

    pep8

--- cachetclient/v1/__init__.py
+++ cachetclient/v1/__init__.py
@@ -7,5 +7,5 @@ from cachetclient.v1.incidents import Incident  # noqa
 from cachetclient.v1.incident_updates import IncidentUpdate  # noqa
 from cachetclient.v1.metrics import Metric  # noqa
 from cachetclient.v1.metric_points import MetricPoint  # noqa
-from cachetclient.v1.schedules import Schedule
+from cachetclient.v1.schedules import Schedule  # noqa
 from cachetclient.v1 import enums  # noqa
